821787f89f957f9f46f2093f79816c94ccc3fa23
Shrawansinh prankada <147142619+Shrawansinh@users.noreply.github.com>
2024-03-03 23:24:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("Welcome to the grow with Shrawan");
+let a=console.error("shrawan");
+let name=prompt("Enter the name:");
+let subname=prompt("Enter the subname:");
+
+let c=console.log("Welcome",name,"",subname,"","!");
+alert(c);
